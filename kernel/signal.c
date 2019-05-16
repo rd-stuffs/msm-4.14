cec3752424a6c8c0baa187b6be357ab560ba094c
Roman Gushchin <guro@fb.com>
2019-05-16 10:38:21 -0700

    signal: Unconditionally leave the frozen state in ptrace_stop()
    Alex Xu reported a regression in strace, caused by the introduction of
    the cgroup v2 freezer. The regression can be reproduced by stracing
    the following simple program:
    
      #include <unistd.h>
    
      int main() {
          write(1, "a", 1);
          return 0;
      }
    
    An attempt to run strace ./a.out leads to the infinite loop:
      [ pre-main omitted ]
      write(1, "a", 1)                        = ? ERESTARTSYS (To be restarted if SA_RESTART is set)
      write(1, "a", 1)                        = ? ERESTARTSYS (To be restarted if SA_RESTART is set)
      write(1, "a", 1)                        = ? ERESTARTSYS (To be restarted if SA_RESTART is set)
      write(1, "a", 1)                        = ? ERESTARTSYS (To be restarted if SA_RESTART is set)
      write(1, "a", 1)                        = ? ERESTARTSYS (To be restarted if SA_RESTART is set)
      write(1, "a", 1)                        = ? ERESTARTSYS (To be restarted if SA_RESTART is set)
      [ repeats forever ]
    
    The problem occurs because the traced task leaves ptrace_stop()
    (and the signal handling loop) with the frozen bit set. So let's
    call cgroup_leave_frozen(true) unconditionally after sleeping
    in ptrace_stop().
    
    With this patch applied, strace works as expected:
      [ pre-main omitted ]
      write(1, "a", 1)                        = 1
      exit_group(0)                           = ?
      +++ exited with 0 +++
    
    Bug: 154548692
    Fixes: 76f969e8948d ("cgroup: cgroup v2 freezer")
    Change-Id: If644b15ead36ce13f0c2c3dd57eebe3658e3edf7
    Cc: Tejun Heo <tj@kernel.org>
    Acked-by: Oleg Nesterov <oleg@redhat.com>
    Reported-by: Alex Xu <alex_y_xu@yahoo.ca>
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Marco Ballesio <balejs@google.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -1951,6 +1951,7 @@ static void ptrace_stop(int exit_code, int why, int clear_code, siginfo_t *info)
 		read_unlock(&tasklist_lock);
 		cgroup_enter_frozen();
 		freezable_schedule();
+		cgroup_leave_frozen(true);
 	} else {
 		/*
 		 * By the time we got the lock, our tracer went away.
