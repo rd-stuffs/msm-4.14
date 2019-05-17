9be57f3582fb74ad44d86a36d09cd700e82091fd
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-16 23:03:33 -0700

    qos: Execute notifier callbacks atomically
    Allowing the pm_qos notifier callbacks to execute without holding
    pm_qos_lock can cause the callbacks to misbehave, e.g. the cpuidle
    callback could erroneously send more IPIs than necessary.
    
    Fix this by executing the pm_qos callbacks while pm_qos_lock is held.
    
    Change-Id: I0f5b0de2b022997a8f7d88755d7b60070b9a091d
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -377,10 +377,6 @@ static __always_inline int pm_qos_update_target_cpus(struct pm_qos_constraints *
 	pm_qos_set_value(c, curr_value);
 	ret = pm_qos_set_value_for_cpus(req, c, &cpus, new_cpus, action);
 
-	spin_unlock(&pm_qos_lock);
-
-	trace_pm_qos_update_target(action, prev_value, curr_value);
-
 	/*
 	 * if cpu mask bits are set, call the notifier call chain
 	 * to update the new qos restriction for the cores
@@ -394,6 +390,11 @@ static __always_inline int pm_qos_update_target_cpus(struct pm_qos_constraints *
 	} else {
 		ret = 0;
 	}
+
+	spin_unlock(&pm_qos_lock);
+
+	trace_pm_qos_update_target(action, prev_value, curr_value);
+
 	return ret;
 }
 
