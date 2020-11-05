0531ccb696c30b2178ac92380265ed4866620015
Urvashi Agrawal <urvaagra@codeaurora.org>
2020-11-05 12:19:18 -0800

    msm: kgsl: Add a Kconfig setting for GPU idle timeout
    Add a Kconfig option so that we can configure the
    timeout for GPU to go into slumber per-environment
    basis.
    
    Change-Id: I36a2d78a4c987e635160db3056aab7b0d6af68a6
    Signed-off-by: Urvashi Agrawal <urvaagra@codeaurora.org>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1017,9 +1017,7 @@ static int adreno_of_get_power(struct adreno_device *adreno_dev,
 	device->pwrctrl.pm_qos_cpu_mask_latency = 1000;
 	device->pwrctrl.pm_qos_wakeup_latency = 100;
 
-	if (of_property_read_u32(node, "qcom,idle-timeout", &timeout))
-		timeout = 80;
-
+	timeout = CONFIG_QCOM_KGSL_IDLE_TIMEOUT;
 	device->pwrctrl.interval_timeout = msecs_to_jiffies(timeout);
 
 	device->pwrctrl.bus_control = of_property_read_bool(node,
