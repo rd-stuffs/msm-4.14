657f562d01c20d0e3b80a65059a1f36d5300bc1a
Al Viro <viro@zeniv.linux.org.uk>
2021-01-04 15:25:34 -0500

    umount: Move the flag validity checks first
    Unfortunately, there's userland code that used to rely upon these
    checks being done before anything else to check for UMOUNT_NOFOLLOW
    support.  That broke in 41525f56e256 ("fs: refactor ksys_umount").
    Separate those from the rest of checks and move them to ksys_umount();
    unlike everything else in there, this can be sanely done there.
    
    Fixes: 41525f56e256 ("fs: refactor ksys_umount")
    Change-Id: I43718b9dcd88652b2e865a715a55d617759c46fc
    Reported-by: Sargun Dhillon <sargun@sargun.me>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1743,8 +1743,6 @@ static int can_umount(const struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
 
-	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
-		return -EINVAL;
 	if (!may_mount())
 		return -EPERM;
 	if (path->dentry != path->mnt->mnt_root)
@@ -1758,6 +1756,7 @@ static int can_umount(const struct path *path, int flags)
 	return 0;
 }
 
+// caller is responsible for flags being sane
 int path_umount(struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
@@ -1789,6 +1788,7 @@ SYSCALL_DEFINE2(umount, char __user *, name, int, flags)
 	int lookup_flags = 0;
 	bool user_request = !(current->flags & PF_KTHREAD);
 
+	// basic validity checks done first
 	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
 		return -EINVAL;
 
