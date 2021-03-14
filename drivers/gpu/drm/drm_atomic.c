9fe7daddd49e0697b11ae93f37a9ebd4a971389a
Yaroslav Furman <yaro330@gmail.com>
2021-03-15 01:34:13 +0300

    drm: Boost CPU upon running an atomic ioctl
    Change-Id: I10b0bbb2e4f65573658a9510c6cf34ea59244645
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -32,6 +32,7 @@
 #include <drm/drm_print.h>
 #include <linux/pm_qos.h>
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 #include <linux/sync_file.h>
 
 #include "drm_crtc_internal.h"
@@ -2265,6 +2266,7 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
+		cpu_input_boost_kick();
 	}
 
 	drm_modeset_acquire_init(&ctx, 0);
