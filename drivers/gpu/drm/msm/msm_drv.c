fa405cc623e1e11bbd55395d5a8d3dfd3305b73d
Deven Solanki <quic_dsolanki@quicinc.com>
2023-01-03 11:44:54 +0530

    disp: msm: clear platform device drvdata on msm_drm bind fail
    For msm_drm_bind() fail case, clear driver data pointer from pdev.
    
    Change-Id: I4add058b77a703389c1f1928b90aa09640c2ce6a
    Signed-off-by: Deven Solanki <quic_dsolanki@quicinc.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (c) 2016-2021, The Linux Foundation. All rights reserved.
  * Copyright (C) 2013 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
@@ -950,6 +950,7 @@ mdss_init_fail:
 	kfree(priv);
 priv_alloc_fail:
 	drm_dev_unref(ddev);
+	platform_set_drvdata(pdev, NULL);
 	return ret;
 }
 
