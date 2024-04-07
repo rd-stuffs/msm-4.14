b386e3554649c11bd81e34083eb606b3ddf1a696
Richard Raya <rdxzv.dev@gmail.com>
2024-04-07 19:03:59 -0300

    build.sh: Switch to StormBreaker
    Change-Id: Ibbc97e0397e64ebe4a3a59405818028a74490dbb
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #
-# Compile script for QuicksilveR kernel
+# Compile script for StormBreaker kernel
 # Copyright (C) 2020-2021 Adithya R.
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="QuicksilveR-surya-$(date '+%Y%m%d-%H%M').zip"
-TC_DIR="$HOME/tc/azure-clang"
-AK3_DIR="$HOME/android/AnyKernel3"
+ZIPNAME="StormBreaker-4.14-X15.1-Surya-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="$(pwd)/tc/clang-r498229"
+AK3_DIR="$(pwd)/android/AnyKernel3"
 DEFCONFIG="vendor/surya-perf_defconfig"
 
 export PATH="$TC_DIR/bin:$PATH"
 
 if ! [ -d "$TC_DIR" ]; then
-	echo "Azure clang not found! Cloning to $TC_DIR..."
-	if ! git clone --depth=1 -b main https://gitlab.com/Panchajanya1999/azure-clang "$TC_DIR"; then
+	echo "AOSP clang not found! Cloning to $TC_DIR..."
+	if ! git clone --depth=1 -b 17 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone.git "$TC_DIR"; then
 		echo "Cloning failed! Aborting..."
 		exit 1
 	fi
@@ -34,32 +34,30 @@ mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz dtbo.img
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 Image dtb.img dtbo.img
 
-kernel="out/arch/arm64/boot/Image.gz"
-dtb="out/arch/arm64/boot/dts/qcom/sdmmagpie.dtb"
+kernel="out/arch/arm64/boot/Image"
+dtb="out/arch/arm64/boot/dtb.img"
 dtbo="out/arch/arm64/boot/dtbo.img"
 
 if [ -f "$kernel" ] && [ -f "$dtb" ] && [ -f "$dtbo" ]; then
 	echo -e "\nKernel compiled succesfully! Zipping up...\n"
 	if [ -d "$AK3_DIR" ]; then
 		cp -r $AK3_DIR AnyKernel3
-	elif ! git clone -q https://github.com/ghostrider-reborn/AnyKernel3 -b surya; then
+	elif ! git clone -q https://github.com/stormbreaker-project/AnyKernel3 -b surya; then
 		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
 		exit 1
 	fi
-	cp $kernel $dtbo AnyKernel3
-	cp $dtb AnyKernel3/dtb
+	cp $kernel $dtb $dtbo AnyKernel3
 	rm -rf out/arch/arm64/boot
 	cd AnyKernel3
 	git checkout surya &> /dev/null
-	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+	sed -i '/supported.versions=10-13/d' anykernel.sh
+	zip -r9 "../$ZIPNAME" * -x .git Makefile README.md *placeholder
 	cd ..
 	rm -rf AnyKernel3
 	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
 	echo "Zip: $ZIPNAME"
-	curl --upload-file "$ZIPNAME" http://transfer.sh/"$ZIPNAME"
-	echo
 else
 	echo -e "\nCompilation failed!"
 	exit 1
