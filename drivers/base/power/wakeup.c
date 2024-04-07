b3cd2b46b579da91ef136f9fe87a62de0519f5d5
Richard Raya <rdxzv.dev@gmail.com>
2024-04-07 19:42:10 -0300

    wakeup: Fix int-conversion
    Change-Id: I0e7d126f3a9fc9266fc1f912d6b115c971d525c8
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -1173,9 +1173,10 @@ static const struct file_operations wakeup_sources_stats_fops = {
 
 static int __init wakeup_sources_debugfs_init(void)
 {
+	int value = 1;
 	wakeup_sources_stats_dentry = debugfs_create_file("wakeup_sources",
 			S_IRUGO, NULL, NULL, &wakeup_sources_stats_fops);
-	debugfs_create_file("trace_marker", 0220, debugfs_create_dir("tracing", NULL), 1, NULL);
+	debugfs_create_file("trace_marker", 0220, debugfs_create_dir("tracing", NULL), &value, NULL);
         debugfs_create_dir("events",debugfs_create_dir("tracing", NULL));
 	return 0;
 }
