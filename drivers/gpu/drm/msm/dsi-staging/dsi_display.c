1a4339699ff9eabba6a892e0bb00b5493e7c7849
Richard Raya <rdxzv.dev@gmail.com>
2024-04-13 20:26:13 -0300

    Revert "drm/msm: dsi: Dynamically adjust WALT window with refresh rate"
    This reverts commit 50bedb6552a465b2061874e35f640be673ad31ce.
    
    Change-Id: Ia80bb6a6d21e3f9d87abc897985787fef663b2c6
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -18,7 +18,6 @@
 #include <linux/of.h>
 #include <linux/of_gpio.h>
 #include <linux/err.h>
-#include <linux/sched.h>
 #include <linux/battery_saver.h>
 
 #include "msm_drv.h"
@@ -6840,7 +6839,6 @@ int dsi_display_validate_mode_change(struct dsi_display *display,
 			dsi_panel_get_dfps_caps(display->panel, &dfps_caps);
 			if (cur_mode->timing.refresh_rate != adj_mode->timing.refresh_rate) {
 				WRITE_ONCE(cur_refresh_rate, adj_mode->timing.refresh_rate);
-				sched_set_refresh_rate(adj_mode->timing.refresh_rate);
 			}
 			if (dfps_caps.dfps_support ||
 			    dyn_clk_caps->maintain_const_fps) {
@@ -7810,7 +7808,6 @@ int dsi_display_enable(struct dsi_display *display)
 
 	mode = display->panel->cur_mode;
 	WRITE_ONCE(cur_refresh_rate, mode->timing.refresh_rate);
-	sched_set_refresh_rate(mode->timing.refresh_rate);
 
 	if (mode->dsi_mode_flags & DSI_MODE_FLAG_DMS) {
 		rc = dsi_panel_switch(display->panel);
@@ -7986,9 +7983,6 @@ int dsi_display_disable(struct dsi_display *display)
 	}
 
 	mutex_unlock(&display->display_lock);
-
-	sched_set_refresh_rate(60);
-
 	SDE_EVT32(SDE_EVTLOG_FUNC_EXIT);
 	return rc;
 }
