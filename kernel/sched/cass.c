eedb17be52760fb0ac4aefc4a92fa5c382821582
Alexander Winkowski <dereference23@outlook.com>
2024-05-15 17:18:56 +0000

    sched/cass: Skip reserved cpus
    Change-Id: I77e5663fa00afba2211b52997e007a0f2e6364e2
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -123,6 +123,9 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync)
 		struct cpuidle_state *idle_state;
 		struct rq *rq = cpu_rq(cpu);
 
+		if (is_reserved(cpu))
+			continue;
+
 		/*
 		 * Check if this CPU is idle or only has SCHED_IDLE tasks. For
 		 * sync wakes, treat the current CPU as idle if @current is the
