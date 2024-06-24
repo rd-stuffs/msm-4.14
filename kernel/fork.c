3db01c208ff866d0f7b07bc5e4299b2e0c79725d
Richard Raya <rdxzv.dev@gmail.com>
2024-06-24 00:06:52 -0300

    kernel: Boost CPU for a short period when zygote forks
    Change-Id: I74568b295bd52546a9580365c137ba0a066eb8a7
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -96,6 +96,7 @@
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2246,10 +2247,11 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
+	/* Boost to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
 		devfreq_boost_kick_max(DEVFREQ_CPU_CPU_LLCC_BW, 50);
+		cpu_input_boost_kick_max(50);
 	}
 
 	/*
