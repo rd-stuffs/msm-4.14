479b3ff753647337cc320802d12d6709e6d2fe56
Dark-Matter7232 <me@const.eu.org>
2024-07-07 16:45:25 +0530

    qcacld-3.0: Enable threaded napi
    Change-Id: Iad838615d82eebd050c9a28b167f4bf3163ec0d2
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/staging/qca-wifi-host-cmn/hif/src/hif_exec.c
+++ drivers/staging/qca-wifi-host-cmn/hif/src/hif_exec.c
@@ -697,9 +697,9 @@ static struct hif_exec_context *hif_exec_napi_create(uint32_t scale)
 	ctx->exec_ctx.scale_bin_shift = scale;
 	qdf_net_if_create_dummy_if((struct qdf_net_if *)&ctx->netdev);
 	netif_napi_add(&(ctx->netdev), &(ctx->napi), hif_exec_poll,
-		       QCA_NAPI_BUDGET);
+		        QCA_NAPI_BUDGET);
+	dev_set_threaded(&(ctx->netdev), true);
 	napi_enable(&ctx->napi);
-
 	return &ctx->exec_ctx;
 }
 #else
