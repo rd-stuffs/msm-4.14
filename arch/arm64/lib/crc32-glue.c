0f8d500013aa25743ee51cc2305c7e01d86aca66
Ard Biesheuvel <ardb@kernel.org>
2024-10-18 09:53:51 +0200

    arm64/crc32: Implement 4-way interleave using PMULL
    Now that kernel mode NEON no longer disables preemption, using FP/SIMD
    in library code which is not obviously part of the crypto subsystem is
    no longer problematic, as it will no longer incur unexpected latencies.
    
    So accelerate the CRC-32 library code on arm64 to use a 4-way
    interleave, using PMULL instructions to implement the folding.
    
    On Apple M2, this results in a speedup of 2 - 2.8x when using input
    sizes of 1k - 8k. For smaller sizes, the overhead of preserving and
    restoring the FP/SIMD register file may not be worth it, so 1k is used
    as a threshold for choosing this code path.
    
    The coefficient tables were generated using code provided by Eric. [0]
    
    [0] https://github.com/ebiggers/libdeflate/blob/master/scripts/gen_crc32_multipliers.c
    
    Adapted to 4.14:
    - cpu_have_named_feature(PMULL) is a 6.x API; use (elf_hwcap & HWCAP_PMULL)
      from <asm/hwcap.h> as the existing arch/arm64/crypto code does.
    - crypto_simd_usable() does not exist here; use may_use_simd() from
      <asm/simd.h>. <crypto/internal/simd.h> is not needed and is omitted.
    
    Change-Id: fbd391b916e7c4dd89d37f78440454b1da5523db
    Link: https://lore.kernel.org/r/20241018075347.2821102-8-ardb+git@google.com
    Cc: Eric Biggers <ebiggers@kernel.org>
    Reviewed-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- arch/arm64/lib/crc32-glue.c
+++ arch/arm64/lib/crc32-glue.c
@@ -1,23 +1,71 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include <linux/crc32.h>
+#include <linux/kernel.h>
 #include <linux/linkage.h>
+
+#include <asm/hwcap.h>
+#include <asm/neon.h>
+#include <asm/simd.h>
+
+// The minimum input length to consider the 4-way interleaved code path
+static const size_t min_len = 1024;
 
 asmlinkage u32 crc32_le_arm64(u32 crc, unsigned char const *p, size_t len);
 asmlinkage u32 crc32c_le_arm64(u32 crc, unsigned char const *p, size_t len);
 asmlinkage u32 crc32_be_arm64(u32 crc, unsigned char const *p, size_t len);
 
+asmlinkage u32 crc32_le_arm64_4way(u32 crc, unsigned char const *p, size_t len);
+asmlinkage u32 crc32c_le_arm64_4way(u32 crc, unsigned char const *p, size_t len);
+asmlinkage u32 crc32_be_arm64_4way(u32 crc, unsigned char const *p, size_t len);
+
 u32 __pure crc32_le(u32 crc, unsigned char const *p, size_t len)
 {
+	if (len >= min_len && (elf_hwcap & HWCAP_PMULL) && may_use_simd()) {
+		kernel_neon_begin();
+		crc = crc32_le_arm64_4way(crc, p, len);
+		kernel_neon_end();
+
+		p += round_down(len, 64);
+		len %= 64;
+
+		if (!len)
+			return crc;
+	}
+
 	return crc32_le_arm64(crc, p, len);
 }
 
 u32 __pure __crc32c_le(u32 crc, unsigned char const *p, size_t len)
 {
+	if (len >= min_len && (elf_hwcap & HWCAP_PMULL) && may_use_simd()) {
+		kernel_neon_begin();
+		crc = crc32c_le_arm64_4way(crc, p, len);
+		kernel_neon_end();
+
+		p += round_down(len, 64);
+		len %= 64;
+
+		if (!len)
+			return crc;
+	}
+
 	return crc32c_le_arm64(crc, p, len);
 }
 
 u32 __pure crc32_be(u32 crc, unsigned char const *p, size_t len)
 {
+	if (len >= min_len && (elf_hwcap & HWCAP_PMULL) && may_use_simd()) {
+		kernel_neon_begin();
+		crc = crc32_be_arm64_4way(crc, p, len);
+		kernel_neon_end();
+
+		p += round_down(len, 64);
+		len %= 64;
+
+		if (!len)
+			return crc;
+	}
+
 	return crc32_be_arm64(crc, p, len);
 }
