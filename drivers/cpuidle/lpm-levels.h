89a65274b2383ff505abded0239e7e77120981d6
Richard Raya <rdxzv.dev@gmail.com>
2024-12-06 23:52:15 -0300

    Revert "cpuidle: lpm-levels: Remove sleep bias feature"
    This reverts commit 5bdb5354eab9faa3e9c52e5c6d1aa2398677c499.
    
    Change-Id: If69d0b201de8a1ae085b35734f1010f1b61a863b
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/cpuidle/lpm-levels.h
+++ drivers/cpuidle/lpm-levels.h
@@ -37,6 +37,7 @@ struct lpm_cpu {
 	int nlevels;
 	unsigned int psci_mode_shift;
 	unsigned int psci_mode_mask;
+	uint64_t bias;
 	struct cpuidle_driver *drv;
 	struct lpm_cluster *parent;
 };
@@ -85,6 +86,7 @@ struct lpm_cluster {
 	struct lpm_stats *stats;
 	unsigned int psci_mode_shift;
 	unsigned int psci_mode_mask;
+	struct hrtimer histtimer;
 };
 
 struct lpm_cluster *lpm_of_parse_cluster(struct platform_device *pdev);
