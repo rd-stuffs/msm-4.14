20540b834c6b9658ff057ad2f35b96edffdf0287
Richard Raya <rdxzv.dev@gmail.com>
2024-12-19 00:56:59 -0300

    sched/cass: Checkout again
    Change-Id: Ib7993c14e7d3ffa354be744629593a8646f55efa
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2023-2024 Sultan Alsawaf <sultan@kerneltoast.com>.
+ * Copyright (C) 2023 Sultan Alsawaf <sultan@kerneltoast.com>.
  */
 
 /**
@@ -33,13 +33,13 @@ struct cass_cpu_cand {
 };
 
 static __always_inline
-unsigned long cass_cpu_util(int cpu, int this_cpu, bool sync)
+unsigned long cass_cpu_util(int cpu, bool sync)
 {
 	struct cfs_rq *cfs_rq = &cpu_rq(cpu)->cfs;
 	unsigned long util = READ_ONCE(cfs_rq->avg.util_avg);
 
 	/* Deduct @current's util from this CPU if this is a sync wake */
-	if (sync && cpu == this_cpu)
+	if (sync && cpu == smp_processor_id())
 		lsub_positive(&util, task_util(current));
 
 	if (sched_feat(UTIL_EST))
@@ -52,24 +52,20 @@ unsigned long cass_cpu_util(int cpu, int this_cpu, bool sync)
 /* Returns true if @a is a better CPU than @b */
 static __always_inline
 bool cass_cpu_better(const struct cass_cpu_cand *a,
-		     const struct cass_cpu_cand *b, unsigned long p_util,
-		     int this_cpu, int prev_cpu, bool sync)
+		     const struct cass_cpu_cand *b,
+		     int prev_cpu, bool sync)
 {
 #define cass_cmp(a, b) ({ res = (a) - (b); })
 #define cass_eq(a, b) ({ res = (a) == (b); })
 	long res;
-
-	/* Prefer the CPU that fits the task */
-	if (cass_cmp(fits_capacity(p_util, a->cap),
-		     fits_capacity(p_util, b->cap)))
-		goto done;
 
 	/* Prefer the CPU with lower relative utilization */
 	if (cass_cmp(b->util, a->util))
 		goto done;
 
 	/* Prefer the current CPU for sync wakes */
-	if (sync && (cass_eq(a->cpu, this_cpu) || !cass_cmp(b->cpu, this_cpu)))
+	if (sync && (cass_eq(a->cpu, smp_processor_id()) ||
+		     !cass_cmp(b->cpu, smp_processor_id())))
 		goto done;
 
 	/* Prefer the CPU with higher capacity */
@@ -98,51 +94,46 @@ done:
 static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync)
 {
 	/* Initialize @best such that @best always has a valid CPU at the end */
-	struct cass_cpu_cand cands[2], *best = cands;
-	int this_cpu = raw_smp_processor_id();
+	struct cass_cpu_cand cands[2], *best = cands, *curr;
+	struct cpuidle_state *idle_state;
 	bool has_idle = false;
 	unsigned long p_util;
 	int cidx = 0, cpu;
 
 	/* Get the utilization for this task */
-	p_util = task_util_est(p);
+	p_util = clamp(task_util_est(p),
+		       uclamp_eff_value(p, UCLAMP_MIN),
+		       uclamp_eff_value(p, UCLAMP_MAX));
 
 	/*
-	 * Find the best CPU to wake @p on. The RCU read lock is needed for
-	 * idle_get_state().
-	 *
-	 * Note: @curr->cpu must be initialized before this loop ends. This is
-	 * necessary to ensure @best->cpu contains a valid CPU upon returning;
-	 * otherwise, if only one CPU is allowed and it is skipped before
-	 * @curr->cpu is set, then @best->cpu will be garbage.
+	 * Find the best CPU to wake @p on. Although idle_get_state() requires
+	 * an RCU read lock, an RCU read lock isn't needed because we're not
+	 * preemptible and RCU-sched is unified with normal RCU. Therefore,
+	 * non-preemptible contexts are implicitly RCU-safe.
 	 */
-	rcu_read_lock();
 	for_each_cpu_and(cpu, p->cpus_ptr, cpu_active_mask) {
-		/* Use the free candidate slot for @curr */
-		struct cass_cpu_cand *curr = &cands[cidx];
-		struct cpuidle_state *idle_state;
-		struct rq *rq = cpu_rq(cpu);
-
-		if (is_reserved(cpu))
-			continue;
+		/* Use the free candidate slot */
+		curr = &cands[cidx];
+		curr->cpu = cpu;
 
 		/*
 		 * Check if this CPU is idle or only has SCHED_IDLE tasks. For
-		 * sync wakes, treat the current CPU as idle if @current is the
-		 * only running task.
+		 * sync wakes, always treat the current CPU as idle.
 		 */
-		if ((sync && cpu == this_cpu && rq->nr_running == 1) ||
-		    sched_idle_cpu(cpu)) {
+		if ((sync && cpu == smp_processor_id()) ||
+		    idle_cpu(cpu) || sched_idle_cpu(cpu)) {
 			/* Discard any previous non-idle candidate */
-			if (!has_idle)
+			if (!has_idle) {
 				best = curr;
+				cidx ^= 1;
+			}
 			has_idle = true;
 
 			/* Nonzero exit latency indicates this CPU is idle */
 			curr->exit_lat = 1;
 
 			/* Add on the actual idle exit latency, if any */
-			idle_state = idle_get_state(rq);
+			idle_state = idle_get_state(cpu_rq(cpu));
 			if (idle_state)
 				curr->exit_lat += idle_state->exit_latency;
 		} else {
@@ -155,7 +146,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync)
 		}
 
 		/* Get this CPU's utilization, possibly without @current */
-		curr->util = cass_cpu_util(cpu, this_cpu, sync);
+		curr->util = cass_cpu_util(cpu, sync);
 
 		/*
 		 * Add @p's utilization to this CPU if it's not @p's CPU, to
@@ -165,33 +156,31 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync)
 		if (cpu != task_cpu(p))
 			curr->util += p_util;
 
-		/* Get the original, maximum _possible_ capacity of this CPU */
-		curr->cap = arch_scale_cpu_capacity(NULL, cpu);
+		/*
+		 * Get the current capacity of this CPU adjusted for thermal
+		 * pressure as well as IRQ and RT-task time.
+		 */
+		curr->cap = capacity_of(cpu);
 
 		/* Calculate the relative utilization for this CPU candidate */
 		curr->util = curr->util * SCHED_CAPACITY_SCALE / curr->cap;
 
-		/*
-		 * Check if this CPU is better than the best CPU found so far.
-		 * If @best == @curr then there's no need to compare them, but
-		 * cidx still needs to be changed to the other candidate slot.
-		 */
-		curr->cpu = cpu;
-		if (best == curr ||
-		    cass_cpu_better(curr, best, p_util, this_cpu, prev_cpu,
-				    sync)) {
+		/* If @best == @curr then there's no need to compare them */
+		if (best == curr)
+			continue;
+
+		/* Check if this CPU is better than the best CPU found */
+		if (cass_cpu_better(curr, best, prev_cpu, sync)) {
 			best = curr;
 			cidx ^= 1;
 		}
 	}
-	rcu_read_unlock();
 
 	return best->cpu;
 }
 
 static int cass_select_task_rq_fair(struct task_struct *p, int prev_cpu,
-				    int sd_flag, int wake_flags,
-				    int sibling_count_hint)
+				    int sd_flag, int wake_flags, int sibling_count_hint)
 {
 	bool sync;
 
