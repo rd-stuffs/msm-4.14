tytydraco/android_kernel_oneplus_sm8150

net: Allow BFP JIT to compile without module support
Thanks to kdrag0n @ GitHub for his original commit using vmalloc instead
of kmalloc (preventing a panic).

Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

kernel/bpf/core.c
@@ -498,7 +498,11 @@ bpf_jit_binary_alloc(unsigned int proglen, u8 **image_ptr,
 	 * random section of illegal instructions.
 	 */
 	size = round_up(proglen + sizeof(*hdr) + 128, PAGE_SIZE);
+#ifdef CONFIG_MODULES
 	hdr = module_alloc(size);
+#else
+	hdr = vmalloc_exec(size);
+#endif
 	if (hdr == NULL)
 		return NULL;
 
@@ -518,7 +522,11 @@ bpf_jit_binary_alloc(unsigned int proglen, u8 **image_ptr,
 
 void bpf_jit_binary_free(struct bpf_binary_header *hdr)
 {
+#ifdef CONFIG_MODULES
 	module_memfree(hdr);
+#else
+	vfree(hdr);
+#endif
 }
 
 /* This symbol is only overridden by archs that have different

rd-stuffs/msm-4.14

Change-Id: I336835a0bf9abbbbad0b9a0d299b5c22eaf15abb
Signed-off-by: DennySPb <dennyspb@gmail.com>
Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

kernel/bpf/core.c
@@ -589,12 +589,20 @@ EXPORT_SYMBOL(arch_bpf_jit_check_func);
 
 void *__weak bpf_jit_alloc_exec(unsigned long size)
 {
+#ifdef CONFIG_MODULES
 	return module_alloc(size);
+#else
+	return vmalloc_exec(size);
+#endif
 }
 
 void __weak bpf_jit_free_exec(void *addr)
 {
+#ifdef CONFIG_MODULES
 	module_memfree(addr);
+#else
+	vfree(addr);
+#endif
 }
 
 struct bpf_binary_header *