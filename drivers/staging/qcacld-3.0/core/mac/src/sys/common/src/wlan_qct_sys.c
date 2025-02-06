5460c500bcc5e6531fffa7a15a118850ecff3f4f
Richard Raya <rdxzv.dev@gmail.com>
2025-02-06 02:58:38 -0300

    qcacld-3.0: Remove unused variables
    Change-Id: If1b005827b2cf9b1ceb8ce35a5c119bc46dca425
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/staging/qcacld-3.0/core/mac/src/sys/common/src/wlan_qct_sys.c
+++ drivers/staging/qcacld-3.0/core/mac/src/sys/common/src/wlan_qct_sys.c
@@ -58,9 +58,6 @@ QDF_STATUS sys_build_message_header(SYS_MSG_ID msg_id,
  */
 static void umac_stop_complete_cb(void *user_data)
 {
-	qdf_event_t *stop_evt = (qdf_event_t *) user_data;
-	QDF_STATUS qdf_status = qdf_event_set(stop_evt);
-
 	QDF_ASSERT(QDF_IS_STATUS_SUCCESS(qdf_status));
 }
 
