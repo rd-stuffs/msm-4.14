039877dacb73c0bc87128cd9d3861c00595e6ef8
Richard Raya <rdxzv.dev@gmail.com>
2025-02-08 23:08:30 -0300

    sched: Implement se_weight() from 6.1
    Change-Id: I04d3a7c68826f406a987876d4877198275bc0510
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -707,6 +707,13 @@ struct dl_rq {
 };
 
 #ifdef CONFIG_SMP
+/*
+ * XXX we want to get rid of these helpers and use the full load resolution.
+ */
+static inline long se_weight(struct sched_entity *se)
+{
+	return scale_load_down(se->load.weight);
+}
 
 static inline bool sched_asym_prefer(int a, int b)
 {
