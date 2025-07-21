0a2e46b8cadbf8b48ff29e8da54f5a7d4fe4f017
Tao Xue <xuetao09@huawei.com>
2025-07-21 17:39:08 +0800

    UPSTREAM: usb: gadget: Fix use-after-free in composite_dev_cleanup()
    Upstream commit: 151c0aa896c47a4459e07fee7d4843f44c1bb18e
    
    1. In func configfs_composite_bind() -> composite_os_desc_req_prepare():
    if kmalloc fails, the pointer cdev->os_desc_req will be freed but not
    set to NULL. Then it will return a failure to the upper-level function.
    2. in func configfs_composite_bind() -> composite_dev_cleanup():
    it will checks whether cdev->os_desc_req is NULL. If it is not NULL, it
    will attempt to use it.This will lead to a use-after-free issue.
    
    BUG: KASAN: use-after-free in composite_dev_cleanup+0xf4/0x2c0
    Read of size 8 at addr 0000004827837a00 by task init/1
    
    CPU: 10 PID: 1 Comm: init Tainted: G           O      5.10.97-oh #1
     kasan_report+0x188/0x1cc
     __asan_load8+0xb4/0xbc
     composite_dev_cleanup+0xf4/0x2c0
     configfs_composite_bind+0x210/0x7ac
     udc_bind_to_driver+0xb4/0x1ec
     usb_gadget_probe_driver+0xec/0x21c
     gadget_dev_desc_UDC_store+0x264/0x27c
    
    Fixes: 37a3a533429e ("usb: gadget: OS Feature Descriptors support")
    Cc: stable <stable@kernel.org>
    Signed-off-by: Tao Xue <xuetao09@huawei.com>
    Link: https://lore.kernel.org/r/20250721093908.14967-1-xuetao09@huawei.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Quince <quinceroms@gmail.com>
    Change-Id: Ifc210304eea716644d7893d644e42659e47665eb
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -2373,6 +2373,7 @@ int composite_os_desc_req_prepare(struct usb_composite_dev *cdev,
 	if (!cdev->os_desc_req->buf) {
 		ret = -ENOMEM;
 		usb_ep_free_request(ep0, cdev->os_desc_req);
+		cdev->os_desc_req = NULL;
 		goto end;
 	}
 	cdev->os_desc_req->context = cdev;
