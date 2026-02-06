b3114e661003da2b9f3424272c3430b249fa7f9d
Richard Raya <rdxzv.dev@gmail.com>
2026-02-05 23:37:15 -0300

    Revert "gpio: aspeed: Use devm_clk api to manage clock source"
    We're going to revert the v4.14.356-openela clk patchset,
    which adds devm_clk_get_enabled, so revert it.
    
    This reverts commit 334fd09b0e005beb9df7c5013ae0344c1c8861aa.
    
    Change-Id: I7418eff6a984c84045ff6a97ef68cb00c64d0103
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/gpio/gpio-aspeed.c
+++ drivers/gpio/gpio-aspeed.c
@@ -833,7 +833,7 @@ static int __init aspeed_gpio_probe(struct platform_device *pdev)
 	if (!gpio_id)
 		return -EINVAL;
 
-	gpio->clk = devm_clk_get_enabled(&pdev->dev, NULL);
+	gpio->clk = of_clk_get(pdev->dev.of_node, 0);
 	if (IS_ERR(gpio->clk)) {
 		dev_warn(&pdev->dev,
 				"Failed to get clock from devicetree, debouncing disabled\n");
