e782ea873cbe4182198082edb2b5be980bf653c1
Richard Raya <rdxzv.dev@gmail.com>
2026-02-17 16:28:18 -0300

    f2fs: Reduce max reclaim prefree segments
    Limit GC reclaim batch size to reduce potential I/O spikes.
    
    Change-Id: I3491badd28fc0685160b1077aaf002ffc36c79c8
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- fs/f2fs/segment.h
+++ fs/f2fs/segment.h
@@ -13,7 +13,7 @@
 #define NULL_SECNO			((unsigned int)(~0))
 
 #define DEF_RECLAIM_PREFREE_SEGMENTS	5	/* 5% over total segments */
-#define DEF_MAX_RECLAIM_PREFREE_SEGMENTS	4096	/* 8GB in maximum */
+#define DEF_MAX_RECLAIM_PREFREE_SEGMENTS	1024	/* 8GB in maximum */
 
 #define F2FS_MIN_SEGMENTS	9 /* SB + 2 (CP + SIT + NAT) + SSA + MAIN */
 #define F2FS_MIN_META_SEGMENTS	8 /* SB + 2 (CP + SIT + NAT) + SSA */
