9a845a1f4fd7c1f8088a25df0e9222f591d6fa4f
Richard Raya <rdxzv.dev@gmail.com>
2026-02-20 15:49:20 -0300

    msm: kgsl: Use SCHED_RR in place of SCHED_FIFO
    Change-Id: I89b614b7e47966586b988672621f37a9a40961f2
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5336,7 +5336,7 @@ static int __init kgsl_core_init(void)
 			"kgsl_low_prio_worker_thread", false)))
 		goto err;
 
-	sched_setscheduler(kgsl_driver.worker_thread, SCHED_FIFO, &param);
+	sched_setscheduler(kgsl_driver.worker_thread, SCHED_RR, &param);
 
 	kgsl_events_init();
 
