77db62ba0ab55e7610cb5a1ba070665190530e79
Richard Raya <rdxzv.dev@gmail.com>
2026-02-20 20:34:11 -0300

    sched/fair: Fine tune capacity margins
    Change-Id: Ic891620f5b947f92839f4408dd1ae10e8dba0d5f
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -209,13 +209,13 @@ unsigned int capacity_margin				= 1280;
 
 /* Migration margins */
 unsigned int sysctl_sched_capacity_margin_up[MAX_MARGIN_LEVELS] = {
-			[0 ... MAX_MARGIN_LEVELS-1] = 1384}; /* ~26% margin */
+			[0 ... MAX_MARGIN_LEVELS-1] = 1402}; /* 73% margin */
 unsigned int sysctl_sched_capacity_margin_down[MAX_MARGIN_LEVELS] = {
-			[0 ... MAX_MARGIN_LEVELS-1] = 2695}; /* ~62% margin */
+			[0 ... MAX_MARGIN_LEVELS-1] = 2767}; /* 37% margin */
 unsigned int sched_capacity_margin_up[NR_CPUS] = {
-			[0 ... NR_CPUS-1] = 1384}; /* ~26% margin */
+			[0 ... NR_CPUS-1] = 1402}; /* 73% margin */
 unsigned int sched_capacity_margin_down[NR_CPUS] = {
-			[0 ... NR_CPUS-1] = 2695}; /* ~62% margin */
+			[0 ... NR_CPUS-1] = 2767}; /* 37% margin */
 
 #ifdef CONFIG_SCHED_WALT
 /* 1ms default for 20ms window size scaled to 1024 */
