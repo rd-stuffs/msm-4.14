rd-stuffs/msm-4.14

commit 06a6040a424702a6cb2e0da3ded200067aaf3e51

power: smb5-lib: Allow user to skip thermal throttling when charging
* Current can be capped by some other sources. Don't bother with them
for now.

Change-Id: I6e9f9072ed5bb19dfc53732242bcfac4ce688448
Signed-off-by: msdx321 <msdx321@gmail.com>
Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

drivers/power/supply/qcom/smb5-lib.c
@@ -21,6 +21,7 @@
 #include <linux/irq.h>
 #include <linux/iio/consumer.h>
 #include <linux/pmic-voter.h>
+#include <linux/module.h>
 #include <linux/of_batterydata.h>
 #include <linux/ktime.h>
 #include <linux/usb/usbpd.h>
@@ -48,6 +49,9 @@
 			pr_debug("%s: %s: " fmt, chg->name,	\
 				__func__, ##__VA_ARGS__);	\
 	} while (0)
+
+bool skip_thermal = false;
+module_param(skip_thermal, bool, 0644);
 
 #define typec_rp_med_high(chg, typec_mode)			\
 	((typec_mode == POWER_SUPPLY_TYPEC_SOURCE_MEDIUM	\
@@ -2835,6 +2839,8 @@ extern int LctThermal;
 int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 				const union power_supply_propval *val)
 {
+	int temp_level;
+
 	if (val->intval < 0)
 		return -EINVAL;
 
@@ -2864,6 +2870,11 @@ int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 
 	chg->system_temp_level = val->intval;
 
+	if (skip_thermal) {
+		temp_level = chg->system_temp_level;
+		chg->system_temp_level = 0;
+	}
+
 	if (chg->system_temp_level == chg->thermal_levels)
 		return vote(chg->chg_disable_votable,
 			THERMAL_DAEMON_VOTER, true, 0);
@@ -2874,6 +2885,11 @@ int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 
 	vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, true,
 			chg->thermal_mitigation[chg->system_temp_level]);
+
+	if (skip_thermal) {
+		chg->system_temp_level = temp_level;
+	}
+
 	return 0;
 }
 

commit a3480b0901d36fee222f595748bd0691e54f0f7c

power: smb5-lib: Fix system_temp_level corruption with skip_thermal
When skip_thermal is enabled, the system_temp_level variable is not
restored on early return paths, leaving it permanently at 0.

This breaks smblib_get_prop_system_temp_level() which returns the
corrupted value to userspace, and prevents restoring the original
thermal level if skip_thermal is later disabled.

Restore system_temp_level before each early return when skip_thermal
is active.

Fixes: 06a6040a4247 ("power: smb5-lib: Allow user to skip thermal throttling when charging")
Change-Id: Ida718faa3c51121ce2fef11412381c02d416edcb
Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

drivers/power/supply/qcom/smb5-lib.c
@@ -2875,20 +2875,25 @@ int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 		chg->system_temp_level = 0;
 	}
 
-	if (chg->system_temp_level == chg->thermal_levels)
+	if (chg->system_temp_level == chg->thermal_levels) {
+		if (skip_thermal)
+			chg->system_temp_level = temp_level;
 		return vote(chg->chg_disable_votable,
 			THERMAL_DAEMON_VOTER, true, 0);
+	}
 
 	vote(chg->chg_disable_votable, THERMAL_DAEMON_VOTER, false, 0);
-	if (chg->system_temp_level == 0)
+	if (chg->system_temp_level == 0) {
+		if (skip_thermal)
+			chg->system_temp_level = temp_level;
 		return vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, false, 0);
+	}
 
 	vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, true,
 			chg->thermal_mitigation[chg->system_temp_level]);
 
-	if (skip_thermal) {
+	if (skip_thermal)
 		chg->system_temp_level = temp_level;
-	}
 
 	return 0;
 }