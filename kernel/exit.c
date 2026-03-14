rd-stuffs/msm-4.14

commit 985fce577f712b67d9a99c084b55fa7fb07607a7

fuse: Import FUSE shortcircuit
Change-Id: I8a54297ee898cd272c7a35082c9c058a76ced3fa
Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>
Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

kernel/exit.c
@@ -232,6 +232,14 @@ void release_task(struct task_struct *p)
 {
 	struct task_struct *leader;
 	int zap_leader;
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	if (p->fpack) {
+		if (p->fpack->iname)
+			__putname(p->fpack->iname);
+		kfree(p->fpack);
+		p->fpack = NULL;
+	}
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 repeat:
 	/* don't need to get the RCU readlock here - the process is dead and
 	 * can't be modifying its own credentials. But shut RCU-lockdep up */

fs/fuse/Kconfig
@@ -26,3 +26,7 @@ config CUSE
 
 	  If you want to develop or use a userspace character device
 	  based on CUSE, answer Y or M.
+
+config FUSE_FS_SHORTCIRCUIT
+	tristate "Support shortcircuit"
+	depends on FUSE_FS

fs/fuse/Makefile
@@ -7,3 +7,4 @@ obj-$(CONFIG_CUSE) += cuse.o
 
 fuse-objs := dev.o dir.o file.o inode.o control.o xattr.o acl.o
 fuse-objs += passthrough.o
+fuse-$(CONFIG_FUSE_FS_SHORTCIRCUIT) += shortcircuit.o

fs/fuse/dev.c
@@ -8,6 +8,10 @@
 
 #include "fuse_i.h"
 
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+#include "fuse_shortcircuit.h"
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/poll.h>
@@ -100,6 +104,12 @@ void fuse_request_free(struct fuse_req *req)
 		kfree(req->pages);
 		kfree(req->page_descs);
 	}
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	if (req->iname) {
+		__putname(req->iname);
+		req->iname = NULL;
+	}
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 	kmem_cache_free(fuse_req_cachep, req);
 }
 
@@ -573,10 +583,20 @@ ssize_t fuse_simple_request(struct fuse_conn *fc, struct fuse_args *args)
 	       args->in.numargs * sizeof(struct fuse_in_arg));
 	req->out.argvar = args->out.argvar;
 	req->out.numargs = args->out.numargs;
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	req->iname = args->iname;
+	args->iname = NULL;
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 	memcpy(req->out.args, args->out.args,
 	       args->out.numargs * sizeof(struct fuse_arg));
 	fuse_request_send(fc, req);
 	ret = req->out.h.error;
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	if (!ret) {
+		if (req->private_lower_rw_file != NULL)
+			args->private_lower_rw_file = req->private_lower_rw_file;
+	}
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 	if (!ret && args->out.argvar) {
 		BUG_ON(args->out.numargs != 1);
 		ret = req->out.args[0].size;
@@ -1361,6 +1381,24 @@ static ssize_t fuse_dev_do_read(struct fuse_dev *fud, struct file *file,
 	__fuse_get_request(req);
 	set_bit(FR_SENT, &req->flags);
 	spin_unlock(&fpq->lock);
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	if (sct_mode == 1) {
+		if (current->fpack) {
+			if (current->fpack->iname)
+				__putname(current->fpack->iname);
+			memset(current->fpack, 0, sizeof(struct fuse_package));
+		}
+		if (req->in.h.opcode == FUSE_OPEN || req->in.h.opcode == FUSE_CREATE) {
+			if (!current->fpack)
+				current->fpack = kzalloc(sizeof(struct fuse_package), GFP_KERNEL);
+			if (likely(current->fpack)) {
+				current->fpack->fuse_open_req = true;
+				current->fpack->iname = req->iname;
+				req->iname = NULL;
+			}
+		}
+	}
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 	/* matches barrier in request_wait_answer() */
 	smp_mb__after_atomic();
 	if (test_bit(FR_INTERRUPTED, &req->flags))
@@ -1892,6 +1930,13 @@ static ssize_t fuse_dev_do_write(struct fuse_dev *fud,
 	struct fuse_req *req;
 	struct fuse_out_header oh;
 
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	if (current->fpack && current->fpack->iname) {
+		__putname(current->fpack->iname);
+		current->fpack->iname = NULL;
+	}
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
+
 	if (nbytes < sizeof(struct fuse_out_header))
 		return -EINVAL;
 
@@ -1963,6 +2008,10 @@ static ssize_t fuse_dev_do_write(struct fuse_dev *fud,
 		req->out.h.error = kern_path(path, 0, req->canonical_path);
 	}
 	fuse_copy_finish(cs);
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+
+	fuse_setup_shortcircuit(fc, req);
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 
 	spin_lock(&fpq->lock);
 	clear_bit(FR_LOCKED, &req->flags);

fs/fuse/dir.c
@@ -467,6 +467,9 @@ static int fuse_create_open(struct inode *dir, struct dentry *entry,
 	struct fuse_open_out outopen;
 	struct fuse_entry_out outentry;
 	struct fuse_file *ff;
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	char *iname;
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 
 	/* Userspace expects S_IFREG in create mode */
 	BUG_ON((mode & S_IFMT) != S_IFREG);
@@ -502,7 +505,26 @@ static int fuse_create_open(struct inode *dir, struct dentry *entry,
 	args.out.args[0].value = &outentry;
 	args.out.args[1].size = sizeof(outopen);
 	args.out.args[1].value = &outopen;
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	args.private_lower_rw_file = NULL;
+	iname = inode_name(dir);
+	if (iname) {
+		/* compose full path */
+		if ((strlen(iname) + entry->d_name.len + 2) <= PATH_MAX) {
+			strlcat(iname, "/", PATH_MAX);
+			strlcat(iname, entry->d_name.name, PATH_MAX);
+		} else {
+			__putname(iname);
+			iname = NULL;
+		}
+	}
+	args.iname = iname;
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 	err = fuse_simple_request(fc, &args);
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	if (args.iname)
+		__putname(args.iname);
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 	if (err)
 		goto out_free_ff;
 
@@ -515,6 +537,10 @@ static int fuse_create_open(struct inode *dir, struct dentry *entry,
 	ff->nodeid = outentry.nodeid;
 	ff->open_flags = outopen.open_flags;
 	fuse_passthrough_setup(fc, ff, &outopen);
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	if (args.private_lower_rw_file != NULL)
+		ff->rw_lower_file = args.private_lower_rw_file;
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 	inode = fuse_iget(dir->i_sb, outentry.nodeid, outentry.generation,
 			  &outentry.attr, entry_attr_timeout(&outentry), 0);
 	if (!inode) {

fs/fuse/file.c
@@ -7,6 +7,9 @@
 */
 
 #include "fuse_i.h"
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+#include "fuse_shortcircuit.h"
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 
 #include <linux/pagemap.h>
 #include <linux/slab.h>
@@ -21,6 +24,41 @@
 
 static const struct file_operations fuse_direct_io_file_operations;
 
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+static int fuse_send_open(struct fuse_conn *fc, u64 nodeid, struct file *file,
+			  int opcode, struct fuse_open_out *outargp,
+			  struct file **lower_file)
+{
+	ssize_t ret;
+	struct fuse_open_in inarg;
+	FUSE_ARGS(args);
+	char *iname = NULL;
+
+	memset(&inarg, 0, sizeof(inarg));
+	inarg.flags = file->f_flags & ~(O_CREAT | O_EXCL | O_NOCTTY);
+	if (!fc->atomic_o_trunc)
+		inarg.flags &= ~O_TRUNC;
+	args.in.h.opcode = opcode;
+	args.in.h.nodeid = nodeid;
+	args.in.numargs = 1;
+	args.in.args[0].size = sizeof(inarg);
+	args.in.args[0].value = &inarg;
+	args.out.numargs = 1;
+	args.out.args[0].size = sizeof(*outargp);
+	args.out.args[0].value = outargp;
+
+	if (opcode == FUSE_OPEN)
+		iname = inode_name(file_inode(file));
+	args.iname = iname;
+
+	ret = fuse_simple_request(fc, &args);
+	if (args.iname)
+		__putname(args.iname);
+	if (args.private_lower_rw_file != NULL)
+		*lower_file = args.private_lower_rw_file;
+	return ret;
+}
+#else
 static int fuse_send_open(struct fuse_conn *fc, u64 nodeid, struct file *file,
 			  int opcode, struct fuse_open_out *outargp)
 {
@@ -42,6 +80,7 @@ static int fuse_send_open(struct fuse_conn *fc, u64 nodeid, struct file *file,
 
 	return fuse_simple_request(fc, &args);
 }
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 
 struct fuse_file *fuse_file_alloc(struct fuse_conn *fc)
 {
@@ -51,6 +90,9 @@ struct fuse_file *fuse_file_alloc(struct fuse_conn *fc)
 	if (unlikely(!ff))
 		return NULL;
 
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	ff->rw_lower_file = NULL;
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 	ff->fc = fc;
 	ff->reserved_req = fuse_request_alloc(0);
 	if (unlikely(!ff->reserved_req)) {
@@ -131,7 +173,12 @@ int fuse_do_open(struct fuse_conn *fc, u64 nodeid, struct file *file,
 		struct fuse_open_out outarg;
 		int err;
 
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+		err = fuse_send_open(fc, nodeid, file, opcode, &outarg,
+				&(ff->rw_lower_file));
+#else
 		err = fuse_send_open(fc, nodeid, file, opcode, &outarg);
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 		if (!err) {
 			ff->fh = outarg.fh;
 			ff->open_flags = outarg.open_flags;
@@ -260,8 +307,10 @@ void fuse_release_common(struct file *file, bool isdir)
 	struct fuse_req *req = ff->reserved_req;
 	int opcode = isdir ? FUSE_RELEASEDIR : FUSE_RELEASE;
 
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	fuse_shortcircuit_release(ff);
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 	fuse_passthrough_release(&ff->passthrough);
-
 	fuse_prepare_release(ff, file->f_flags, opcode);
 
 	if (ff->flock) {
@@ -948,9 +997,13 @@ static ssize_t fuse_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 		if (err)
 			return err;
 	}
-
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	if (ff && ff->rw_lower_file)
+		return fuse_shortcircuit_read_iter(iocb, to);
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 	if (ff->passthrough.filp)
 		return fuse_passthrough_read_iter(iocb, to);
+
 	return generic_file_read_iter(iocb, to);
 }
 
@@ -1196,11 +1249,21 @@ static ssize_t fuse_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 	loff_t endbyte = 0;
 	struct fuse_file *ff = file->private_data;
 
-	if (ff->passthrough.filp)
-		return fuse_passthrough_write_iter(iocb, from);
-
 	if (fuse_is_bad(inode))
 		return -EIO;
+
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	if (ff && ff->rw_lower_file) {
+		/* Update size (EOF optimization) and mode (SUID clearing) */
+		err = fuse_update_attributes(mapping->host, file);
+		if (err)
+			return err;
+
+		return fuse_shortcircuit_write_iter(iocb, from);
+	}
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
+	if (ff->passthrough.filp)
+		return fuse_passthrough_write_iter(iocb, from);
 
 	if (get_fuse_conn(inode)->writeback_cache) {
 		/* Update size (EOF optimization) and mode (SUID clearing) */
@@ -2165,6 +2228,10 @@ static int fuse_file_mmap(struct file *file, struct vm_area_struct *vma)
 {
 	struct fuse_file *ff = file->private_data;
 
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	if (ff->rw_lower_file)
+		return fuse_shortcircuit_mmap(file, vma);
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 	if (ff->passthrough.filp)
 		return fuse_passthrough_mmap(file, vma);
 

fs/fuse/fuse_i.h
@@ -172,6 +172,10 @@ struct fuse_file {
 
 	/** Has flock been performed on this file? */
 	bool flock:1;
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	/* the read write file */
+	struct file *rw_lower_file;
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 };
 
 /** One input argument of a request */
@@ -252,6 +256,11 @@ struct fuse_args {
 		unsigned numargs;
 		struct fuse_arg args[2];
 	} out;
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	/** fuse shortcircuit file  */
+	struct file *private_lower_rw_file;
+	char *iname;
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 };
 
 #define FUSE_ARGS(args) struct fuse_args args = {}
@@ -403,6 +412,11 @@ struct fuse_req {
 
 	/** Request is stolen from fuse_file->reserved_req */
 	struct file *stolen_file;
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	/** fuse shortcircuit file  */
+	struct file *private_lower_rw_file;
+	char *iname;
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 };
 
 struct fuse_iqueue {
@@ -571,6 +585,11 @@ struct fuse_conn {
 
 	/** handle fs handles killing suid/sgid/cap on write/chown/trunc */
 	unsigned handle_killpriv:1;
+
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+	/** Shortcircuited IO. */
+	unsigned shortcircuit_io:1;
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 
 	/*
 	 * The following bitfields are only for optimization purposes
@@ -1032,6 +1051,10 @@ extern const struct xattr_handler *fuse_acl_xattr_handlers[];
 struct posix_acl;
 struct posix_acl *fuse_get_acl(struct inode *inode, int type);
 int fuse_set_acl(struct inode *inode, struct posix_acl *acl, int type);
+
+#ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
+extern int sct_mode;
+#endif /* CONFIG_FUSE_FS_SHORTCIRCUIT */
 
 /* passthrough.c */
 int fuse_passthrough_open(struct fuse_dev *fud, u32 lower_fd);

fs/fuse/fuse_shortcircuit.h
@@ -1,0 +1,35 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _FS_FUSE_SHORCIRCUIT_H
+#define _FS_FUSE_SHORCIRCUIT_H
+
+#include "fuse_i.h"
+
+#include <linux/fuse.h>
+#include <linux/sched/signal.h>
+#include <linux/file.h>
+
+void fuse_setup_shortcircuit(struct fuse_conn *fc, struct fuse_req *req);
+
+ssize_t fuse_shortcircuit_read_iter(struct kiocb *iocb, struct iov_iter *to);
+
+ssize_t fuse_shortcircuit_write_iter(struct kiocb *iocb, struct iov_iter *from);
+
+ssize_t fuse_shortcircuit_mmap(struct file *file, struct vm_area_struct *vma);
+
+void fuse_shortcircuit_release(struct fuse_file *ff);
+
+#endif /* _FS_FUSE_SHORCIRCUIT_H */

commit 909f8330f9ec81ed10293d206fde4bbbaf00d55f

fuse: Fix file handle leak in release_task()
When a task exits with fpack->filp non-NULL, the cleanup code frees
fpack->iname and the fpack structure itself, but does not release the
file reference held in fpack->filp.

This can happen if a task exits while a FUSE open request is pending
(process killed, OOM, etc.), leaving fpack->filp pointing to a file
that was opened by sdcardfs_open() but not yet transferred to the
fuse_file.

Add fput() call for fpack->filp before kfree() to properly release the
file reference on task exit.

Fixes: 985fce577f71 ("fuse: Import FUSE shortcircuit")
Change-Id: Ic623433c122c342b0496f379a4327e086ad6ac22
Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

kernel/exit.c
@@ -234,6 +234,8 @@ void release_task(struct task_struct *p)
 	int zap_leader;
 #ifdef CONFIG_FUSE_FS_SHORTCIRCUIT
 	if (p->fpack) {
+		if (p->fpack->filp)
+			fput(p->fpack->filp);
 		if (p->fpack->iname)
 			__putname(p->fpack->iname);
 		kfree(p->fpack);