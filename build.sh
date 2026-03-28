9180c23f744d06ae69092278a6eb5f5a3eddcc57
Richard Raya <rdxzv.dev@gmail.com>
2026-03-28 20:08:35 -0300

    build.sh: Improve script and CI workflow
    Change-Id: Idcc794f3a227fd44d92bef9ba276366f511658e1
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- build.sh
+++ build.sh
@@ -3,93 +3,94 @@
 # Compile script for FSociety kernel
 # Copyright (C) 2020-2021 Adithya R.
 
-SECONDS=0
+set -euo pipefail
+
+trap 'printf "\nInterrupted.\n"; exit 1' INT
+
 ZIPNAME="FSociety-surya-$(date '+%Y%m%d-%H%M').zip"
 LLVM_REV="22"
 TC_DIR="$(pwd)/tc/clang-$LLVM_REV"
-AK3_DIR="$(pwd)/android/AnyKernel3"
+AK3_DIR="$(pwd)/AnyKernel3"
 DEFCONFIG="surya_defconfig"
 
-if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
-   head=$(git rev-parse --verify HEAD 2>/dev/null); then
-	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
+if git rev-parse --is-inside-work-tree &>/dev/null; then
+	sha=$(git rev-parse --verify HEAD)
+	ZIPNAME="${ZIPNAME::-4}-${sha:0:8}.zip"
 fi
 
 export PATH="$TC_DIR/bin:$PATH"
 
-if ! [ -d "$TC_DIR" ]; then
-	echo "Slim LLVM not found! Cloning to $TC_DIR..."
-	if ! git clone --depth=1 -b $LLVM_REV https://bitbucket.org/rdxzv/clang-standalone.git "$TC_DIR"; then
-		echo "Cloning failed! Aborting..."
-		exit 1
-	fi
+if [ ! -d "$TC_DIR" ]; then
+	printf "Cloning Slim LLVM to %s...\n" "$TC_DIR"
+	git clone --depth=1 -b "$LLVM_REV" \
+		https://bitbucket.org/rdxzv/clang-standalone.git "$TC_DIR"
 fi
 
-if ! [ -d "$AK3_DIR" ]; then
-	echo "AnyKernel3 not found! Cloning to $AK3_DIR..."
-	if ! git clone --depth=1 -b FSociety https://github.com/rd-stuffs/AnyKernel3.git "$AK3_DIR"; then
-		echo "Cloning failed! Aborting..."
-		exit 1
-	fi
+if [ ! -d "$AK3_DIR" ]; then
+	printf "Cloning AnyKernel3 to %s...\n" "$AK3_DIR"
+	git clone --depth=1 -b FSociety \
+		https://github.com/rd-stuffs/AnyKernel3.git "$AK3_DIR"
 fi
 
-if [[ $1 = "-rf" || $1 = "--regen-full" ]]; then
-	make $DEFCONFIG
-	cp out/.config arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated full defconfig at $DEFCONFIG"
+if [[ ${1:-} == -rf || ${1:-} == --regen-full ]]; then
+	make "$DEFCONFIG"
+	cp out/.config arch/arm64/configs/"$DEFCONFIG"
+	printf "\nSuccessfully regenerated full defconfig at %s\n" "$DEFCONFIG"
 	exit
 fi
 
-CLEAN=false
-KSU=false
+CLEAN="false"
+KSU="false"
 
 for arg in "$@"; do
 	case $arg in
-		-c|--clean)
-			CLEAN=true
+		-c | --clean)
+			CLEAN="true"
 			;;
-		-s|--su)
-			KSU=true
+		-s | --su)
+			KSU="true"
 			;;
 		*)
-			echo "Unknown argument: $arg"
+			printf "Unknown argument: %s\n" "$arg"
 			exit 1
 			;;
 	esac
 done
 
-if [[ "$CLEAN" = true ]]; then
+if [[ $CLEAN == "true" ]]; then
+	printf "Cleaning output directory...\n"
 	rm -rf out
 fi
 
-echo -e "\nStarting compilation...\n"
-make $DEFCONFIG
+printf "Building surya defconfig...\n"
+make "$DEFCONFIG" &>/dev/null
 
-if [[ "$KSU" = true ]]; then
-	echo -e "\nBuilding with KernelSU support...\n"
+if [[ $KSU == "true" ]]; then
+	printf "Building KernelSU variant...\n"
 	ZIPNAME="${ZIPNAME/FSociety-surya/FSociety-KSU}"
-	scripts/config --file out/.config -e KSU -e KSU_MANUAL_HOOK
-	make olddefconfig
+	scripts/config --file out/.config -e KSU -e KSU_ALLOWLIST_WORKAROUND
+	make olddefconfig &>/dev/null
 fi
 
-make -j$(nproc --all) LLVM=1 2> >(tee log.txt >&2) || exit $?
+printf "\n"
+SECONDS=0
+make -j"$(nproc --all)" LLVM=1 2> >(tee log.txt >&2)
+BUILD_TIME=$SECONDS
 
 kernel="out/arch/arm64/boot/Image.gz-dtb"
 dtbo="out/arch/arm64/boot/dtbo.img"
 dtbo_miui="out/arch/arm64/boot/dtbo-miui.img"
 
-if [ -f "$kernel" ] && [ -f "$dtbo" ] && [ -f "$dtbo_miui" ]; then
-	echo -e "\nKernel compiled successfully! Zipping up...\n"
-	cp -r $AK3_DIR AnyKernel3
-	cp $kernel $dtbo $dtbo_miui AnyKernel3
-	cd AnyKernel3
-	git checkout FSociety &> /dev/null
-	zip -r9 "../$ZIPNAME" * -x .git modules\* patch\* ramdisk\* README.md *placeholder
-	cd ..
-	rm -rf AnyKernel3
-	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-	echo "Zip: $ZIPNAME"
-else
-	echo -e "\nCompilation failed!"
+if [ ! -f "$kernel" ] || [ ! -f "$dtbo" ] || [ ! -f "$dtbo_miui" ]; then
+	printf "\nMissing build artifacts, aborting.\n"
 	exit 1
 fi
+
+printf "\nKernel compiled successfully! Zipping up...\n"
+cp "$kernel" "$dtbo" "$dtbo_miui" "$AK3_DIR"
+cd "$AK3_DIR"
+zip -r9 "../$ZIPNAME" * -x .git modules\* patch\* ramdisk\* README.md \*placeholder &>/dev/null
+rm -f Image.gz-dtb dtbo.img dtbo-miui.img
+cd ..
+printf "\nCompleted in %d minute(s) and %d second(s)!\n" $((BUILD_TIME / 60)) $((BUILD_TIME % 60))
+printf "Zip: %s\n" "$ZIPNAME"
