03478e309cf6f373ed89d58afdf7a96995d486cb
EmanuelCN <emanuelghub@gmail.com>
2026-04-08 15:57:31 +0300

    schedutil: Remove the 10% util suppression in dvfs_headroom
    Change-Id: I94c1e11b4fb29f1b70585d7e6531f38b39157b97
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -271,7 +271,7 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 static inline unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
 {
 	unsigned long capacity = capacity_orig_of(cpu);
-	unsigned long delta, headroom, min_util;
+	unsigned long delta, headroom;
 
 	if (util >= capacity)
 		return util;
@@ -283,14 +283,6 @@ static inline unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
 	 */
 	delta = capacity - util;
 	headroom = ((delta * delta) >> 12);
-
-	/* 10% of capacity threshold */
-	min_util = capacity / 10;
-
-	/* Suppress boosting below the threshold */
-	if (util < min_util) {
-		headroom = (headroom * util * util) / (min_util * min_util);
-	}
 
 	return util + headroom;
 }
