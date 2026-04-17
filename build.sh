2232cd04f7166b49cb72f9359b80d64ecfdc5f9a
Richard Raya <rdxzv.dev@gmail.com>
2026-04-17 20:26:39 -0300

    build.sh: Switch to Eva GCC
    Change-Id: If29decf515b2d91c582e9b3fb70428cad463aab6
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- build.sh
+++ build.sh
@@ -11,9 +11,11 @@ WD="$(pwd)"
 ZIPNAME="FSociety-surya-$(date '+%Y%m%d-%H%M').zip"
 DEFCONFIG="surya_defconfig"
 
-CLANG_REV="22"
-CLANG_DIR="$WD/tc/clang-$CLANG_REV"
-CLANG_URL="https://bitbucket.org/rdxzv/clang-standalone"
+GCC64_DIR="$WD/tc/gcc-arm64"
+GCC32_DIR="$WD/tc/gcc-arm"
+GCC_RELEASES_API="https://api.github.com/repos/mvaisakh/gcc-build/releases/latest"
+GCC_DOWNLOAD_URL="https://github.com/mvaisakh/gcc-build/releases/download"
+GCC_TAG="$(curl -fsSL "$GCC_RELEASES_API" | grep -m1 '"tag_name"' | cut -d'"' -f4)"
 
 AK3_DIR="$WD/AnyKernel3"
 AK3_URL="https://github.com/rd-stuffs/AnyKernel3"
@@ -23,11 +25,32 @@ if git rev-parse --is-inside-work-tree &>/dev/null; then
 	ZIPNAME="${ZIPNAME::-4}-${SHA:0:8}.zip"
 fi
 
-export PATH="$TC_DIR/bin:$PATH"
+if [ -d "$GCC64_DIR" ] && [ -d "$GCC32_DIR" ]; then
+	GCC_INSTALLED_DATE="$("$GCC64_DIR/bin/aarch64-elf-gcc" --version | grep -o '[0-9]\{8\}')"
+	GCC_INSTALLED_DATE="${GCC_INSTALLED_DATE:6:2}${GCC_INSTALLED_DATE:4:2}${GCC_INSTALLED_DATE:0:4}"
+	if [[ $GCC_INSTALLED_DATE != "$GCC_TAG" ]]; then
+		printf "Eva GCC update available (%s). Update? [y/N] " "$GCC_TAG"
+		read -r GCC_UPDATE
+		if [[ ${GCC_UPDATE,,} == y ]]; then
+			rm -rf "$GCC64_DIR" "$GCC32_DIR"
+		fi
+	fi
+fi
 
-if [ ! -d "$CLANG_DIR" ]; then
-	printf "Cloning Slim LLVM to %s...\n" "$CLANG_DIR"
-	git clone --depth=1 -b $CLANG_REV "$CLANG_URL" "$CLANG_DIR"
+if [ ! -d "$GCC64_DIR" ] || [ ! -d "$GCC32_DIR" ]; then
+	if [ ! -d "$GCC64_DIR" ]; then
+		printf "Downloading Eva GCC arm64 (%s)...\n" "$GCC_TAG"
+		mkdir -p "$GCC64_DIR"
+		curl -fL# "$GCC_DOWNLOAD_URL/$GCC_TAG/eva-gcc-arm64-$GCC_TAG.xz" |
+			tar xf - --strip-components=1 -C "$GCC64_DIR"
+	fi
+
+	if [ ! -d "$GCC32_DIR" ]; then
+		printf "Downloading Eva GCC arm (%s)...\n" "$GCC_TAG"
+		mkdir -p "$GCC32_DIR"
+		curl -fL# "$GCC_DOWNLOAD_URL/$GCC_TAG/eva-gcc-arm-$GCC_TAG.xz" |
+			tar xf - --strip-components=1 -C "$GCC32_DIR"
+	fi
 fi
 
 if [ ! -d "$AK3_DIR" ]; then
@@ -35,8 +58,28 @@ if [ ! -d "$AK3_DIR" ]; then
 	git clone --depth=1 -b FSociety "$AK3_URL" "$AK3_DIR"
 fi
 
+KBUILD_COMPILER_STRING="$("$GCC64_DIR/bin/aarch64-elf-gcc" --version | head -n1)"
+PATH="$GCC64_DIR/bin:$GCC32_DIR/bin:$PATH"
+
+export KBUILD_COMPILER_STRING PATH
+
+MAKE=(
+	make
+	CROSS_COMPILE="aarch64-elf-"
+	CROSS_COMPILE_COMPAT="arm-eabi-"
+	LD="$GCC64_DIR/bin/aarch64-elf-ld"
+	AR="aarch64-elf-gcc-ar"
+	AS="aarch64-elf-as"
+	NM="aarch64-elf-nm"
+	OBJDUMP="aarch64-elf-objdump"
+	OBJCOPY="aarch64-elf-objcopy"
+	CC="aarch64-elf-gcc"
+	LLVM=0
+	LLVM_IAS=0
+)
+
 if [[ ${1:-} == -rf || ${1:-} == --regen-full ]]; then
-	make $DEFCONFIG
+	"${MAKE[@]}" $DEFCONFIG
 	cp out/.config arch/arm64/configs/$DEFCONFIG
 	printf "\nSuccessfully regenerated full defconfig at %s\n" $DEFCONFIG
 	exit
@@ -66,18 +109,18 @@ if [[ $CLEAN == "true" ]]; then
 fi
 
 printf "Building surya defconfig...\n"
-make $DEFCONFIG &>/dev/null
+"${MAKE[@]}" $DEFCONFIG &>/dev/null
 
 if [[ $KSU == "true" ]]; then
 	printf "Building KernelSU variant...\n"
 	ZIPNAME="${ZIPNAME/FSociety-surya/FSociety-KSU}"
 	scripts/config --file out/.config -e KSU
-	make olddefconfig &>/dev/null
+	"${MAKE[@]}" olddefconfig &>/dev/null
 fi
 
 printf "\n"
 SECONDS=0
-make -j"$(nproc --all)" LLVM=1 2> >(tee log.txt >&2)
+"${MAKE[@]}" -j"$(nproc --all)" 2> >(tee log.txt >&2)
 BUILD_TIME=$SECONDS
 
 kernel="out/arch/arm64/boot/Image.gz-dtb"
