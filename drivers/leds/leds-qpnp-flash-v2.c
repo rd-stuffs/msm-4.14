373728bfc2f37b93ece3d5eb78778bee0ef207f1
Richard Raya <rdxzv.dev@gmail.com>
2026-04-18 23:43:32 -0300

    msm-4.14: Fix behavioral regressions
    Change-Id: I2f3000da444368bdf6147e416c5adfbfb7461423
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/leds/leds-qpnp-flash-v2.c
+++ drivers/leds/leds-qpnp-flash-v2.c
@@ -1837,10 +1837,9 @@ static void qpnp_flashlight_led_brightness_set(struct led_classdev *led_cdev,
 					flashlight_data->switch_name[i], j, led->snode[j].cdev.name);
 				if(!strcmp(flashlight_data->switch_name[i], led->snode[j].cdev.name)) {
 					rc = qpnp_flash_led_switch_set(&led->snode[j], false);
-					if (rc < 0) {
+					if (rc < 0)
 						pr_err("Failed to set flash LED switch rc=%d\n", rc);
-						break;
-					}
+					break;
 				}
 			}
 		for(i = 0; i < flashlight_data->num_torch; ++i)
@@ -1858,10 +1857,9 @@ static void qpnp_flashlight_led_brightness_set(struct led_classdev *led_cdev,
 					flashlight_data->switch_name[i], j, led->snode[j].cdev.name);
 				if(!strcmp(flashlight_data->switch_name[i], led->snode[j].cdev.name)) {
 					rc = qpnp_flash_led_switch_set(&led->snode[j], value > 0);
-					if (rc < 0) {
+					if (rc < 0)
 						pr_err("Failed to set flash LED switch rc=%d\n", rc);
-						break;
-					}
+					break;
 				}
 			}
 	}
