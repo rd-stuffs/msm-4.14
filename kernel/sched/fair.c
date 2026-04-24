79a2b6e59a20d18d4623a9dab89bc5cd698fbd46
Richard Raya <rdxzv.dev@gmail.com>
2026-04-23 22:38:04 -0300

    sched/fair: Fix sched_nr_latency to match tunable ratio
    sysctl_sched_latency / sysctl_sched_min_granularity = 4000000 / 400000 = 10,
    but sched_nr_latency was hardcoded to 8.
    
    Change-Id: I58f766747e2de677aab885790609a06d876596f8
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -125,7 +125,7 @@ unsigned int normalized_sysctl_sched_min_granularity	= 400000ULL;
 /*
  * This value is kept at sysctl_sched_latency/sysctl_sched_min_granularity
  */
-static unsigned int sched_nr_latency = 8;
+static unsigned int sched_nr_latency = 10;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
