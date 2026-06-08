035afaf9aab542a297542b1add1a72e5a6f2debb
Richard Raya <rdxzv.dev@gmail.com>
2026-06-08 15:46:43 -0300

    Revert "pstore: Select compression at runtime"
    This reverts commit bde83757afca6660272385dcbd6e2d7601fe5316.
    
    Change-Id: Ic368c24e73d64950d19f4b2cddde5beb687e35ac
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- fs/pstore/platform.c
+++ fs/pstore/platform.c
@@ -75,12 +75,6 @@ static DEFINE_SPINLOCK(pstore_lock);
 struct pstore_info *psinfo;
 
 static char *backend;
-static char *compress =
-#ifdef CONFIG_PSTORE_COMPRESS_DEFAULT
-		CONFIG_PSTORE_COMPRESS_DEFAULT;
-#else
-		NULL;
-#endif
 
 /* Compression parameters */
 #ifdef CONFIG_PSTORE_ZLIB_COMPRESS
@@ -88,9 +82,7 @@ static char *compress =
 #define WINDOW_BITS 12
 #define MEM_LEVEL 4
 static struct z_stream_s stream;
-#endif
-#if defined(CONFIG_PSTORE_LZO_COMPRESS)   || \
-    defined(CONFIG_PSTORE_LZ4_COMPRESS)
+#else
 static unsigned char *workspace;
 #endif
 
@@ -271,6 +263,14 @@ static void free_zlib(void)
 	big_oops_buf = NULL;
 	big_oops_buf_sz = 0;
 }
+
+static const struct pstore_zbackend backend_zlib = {
+	.compress	= compress_zlib,
+	.decompress	= decompress_zlib,
+	.allocate	= allocate_zlib,
+	.free		= free_zlib,
+	.name		= "zlib",
+};
 #endif
 
 #ifdef CONFIG_PSTORE_LZO_COMPRESS
@@ -324,6 +324,14 @@ static void free_lzo(void)
 	big_oops_buf = NULL;
 	big_oops_buf_sz = 0;
 }
+
+static const struct pstore_zbackend backend_lzo = {
+	.compress	= compress_lzo,
+	.decompress	= decompress_lzo,
+	.allocate	= allocate_lzo,
+	.free		= free_lzo,
+	.name		= "lzo",
+};
 #endif
 
 #ifdef CONFIG_PSTORE_LZ4_COMPRESS
@@ -381,40 +389,26 @@ static void free_lz4(void)
 	big_oops_buf = NULL;
 	big_oops_buf_sz = 0;
 }
-#endif
 
-static const struct pstore_zbackend *zbackend __ro_after_init;
-
-static const struct pstore_zbackend zbackends[] = {
-#ifdef CONFIG_PSTORE_ZLIB_COMPRESS
-	{
-		.compress	= compress_zlib,
-		.decompress	= decompress_zlib,
-		.allocate	= allocate_zlib,
-		.free		= free_zlib,
-		.name		= "zlib",
-	},
-#endif
-#ifdef CONFIG_PSTORE_LZO_COMPRESS
-	{
-		.compress	= compress_lzo,
-		.decompress	= decompress_lzo,
-		.allocate	= allocate_lzo,
-		.free		= free_lzo,
-		.name		= "lzo",
-	},
-#endif
-#ifdef CONFIG_PSTORE_LZ4_COMPRESS
-	{
-		.compress	= compress_lz4,
-		.decompress	= decompress_lz4,
-		.allocate	= allocate_lz4,
-		.free		= free_lz4,
-		.name		= "lz4",
-	},
-#endif
-	{ }
+static const struct pstore_zbackend backend_lz4 = {
+	.compress	= compress_lz4,
+	.decompress	= decompress_lz4,
+	.allocate	= allocate_lz4,
+	.free		= free_lz4,
+	.name		= "lz4",
 };
+#endif
+
+static const struct pstore_zbackend *zbackend =
+#if defined(CONFIG_PSTORE_ZLIB_COMPRESS)
+	&backend_zlib;
+#elif defined(CONFIG_PSTORE_LZO_COMPRESS)
+	&backend_lzo;
+#elif defined(CONFIG_PSTORE_LZ4_COMPRESS)
+	&backend_lz4;
+#else
+	NULL;
+#endif
 
 static int pstore_compress(const void *in, void *out,
 			   size_t inlen, size_t outlen)
@@ -436,6 +430,7 @@ static int pstore_decompress(void *in, void *out, size_t inlen, size_t outlen)
 static void allocate_buf_for_compression(void)
 {
 	if (zbackend) {
+		pr_info("using %s compression\n", zbackend->name);
 		zbackend->allocate();
 	} else {
 		pr_err("allocate compression buffer error!\n");
@@ -893,25 +888,6 @@ static void pstore_timefunc(unsigned long dummy)
 		mod_timer(&pstore_timer,
 			  jiffies + msecs_to_jiffies(pstore_update_ms));
 }
-
-void __init pstore_choose_compression(void)
-{
-	const struct pstore_zbackend *step;
-
-	if (!compress)
-		return;
-
-	for (step = zbackends; step->name; step++) {
-		if (!strcmp(compress, step->name)) {
-			zbackend = step;
-			pr_info("using %s compression\n", zbackend->name);
-			return;
-		}
-	}
-}
-
-module_param(compress, charp, 0444);
-MODULE_PARM_DESC(compress, "Pstore compression to use");
 
 module_param(backend, charp, 0444);
 MODULE_PARM_DESC(backend, "Pstore backend to use");
