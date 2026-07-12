rd-stuffs/msm-4.14

commit b8a5b603c1e2e4614b6cbab4b5609e2ef53ea73a

sched: Use sched_avg for WALT only
sched_avg stats enabled for SMP, but it is used only in WALT.
So move sched_avg under WALT.

Change-Id: I19b558833e12b80f02fea27c9a9fc8b7630d8689
Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

kernel/sched/sched_avg.c
@@ -176,13 +176,10 @@ unsigned int sched_get_cpu_util(int cpu)
 	util = rq->cfs.avg.util_avg;
 	capacity = capacity_orig_of(cpu);
 
-#ifdef CONFIG_SCHED_WALT
-	if (!walt_disabled && sysctl_sched_use_walt_cpu_util) {
-		util = rq->prev_runnable_sum + rq->grp_time.prev_runnable_sum;
-		util = div64_u64(util,
-				 sched_ravg_window >> SCHED_CAPACITY_SHIFT);
-	}
-#endif
+	util = rq->prev_runnable_sum + rq->grp_time.prev_runnable_sum;
+	util = div64_u64(util,
+			 sched_ravg_window >> SCHED_CAPACITY_SHIFT);
+
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
 
 	util = (util >= capacity) ? capacity : util;

include/linux/sched/stat.h
@@ -22,7 +22,7 @@ extern unsigned long nr_iowait(void);
 extern unsigned long nr_iowait_cpu(int cpu);
 extern void get_iowait_load(unsigned long *nr_waiters, unsigned long *load);
 
-#ifdef CONFIG_SMP
+#ifdef CONFIG_SCHED_WALT
 extern void sched_update_nr_prod(int cpu, long delta, bool inc);
 extern unsigned int sched_get_cpu_util(int cpu);
 extern u64 sched_get_cpu_last_busy_time(int cpu);

kernel/sched/Makefile
@@ -19,9 +19,9 @@ endif
 obj-y += core.o loadavg.o clock.o cputime.o
 obj-y += idle_task.o fair.o rt.o deadline.o
 obj-y += wait.o wait_bit.o swait.o completion.o idle.o
-obj-$(CONFIG_SMP) += cpupri.o cpudeadline.o topology.o stop_task.o sched_avg.o
+obj-$(CONFIG_SMP) += cpupri.o cpudeadline.o topology.o stop_task.o
 obj-$(CONFIG_GENERIC_ARCH_TOPOLOGY) += energy.o
-obj-$(CONFIG_SCHED_WALT) += walt.o boost.o
+obj-$(CONFIG_SCHED_WALT) += walt.o boost.o sched_avg.o
 obj-$(CONFIG_SCHED_AUTOGROUP) += autogroup.o
 obj-$(CONFIG_SCHEDSTATS) += stats.o
 obj-$(CONFIG_SCHED_DEBUG) += debug.o

commit e4db036b6e05cefe721e9c3f47f3f528b4c3c895

sched/walt: Only override util with WALT sums when WALT is active
b8a5b603c1e2 ("sched: Use sched_avg for WALT only") unconditionally
replaced cfs util_avg with rq->prev_runnable_sum + grp_time.prev_runnable_sum,
so PELT util was discarded even when WALT is disabled or
sched_use_walt_cpu_util is off, where those sums are stale.

Fixes: b8a5b603c1e2 ("sched: Use sched_avg for WALT only")
Change-Id: If5a9e35759c8b9105f52ba6cda701259427b9065
Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

kernel/sched/sched_avg.c
@@ -176,9 +176,11 @@ unsigned int sched_get_cpu_util(int cpu)
 	util = rq->cfs.avg.util_avg;
 	capacity = capacity_orig_of(cpu);
 
-	util = rq->prev_runnable_sum + rq->grp_time.prev_runnable_sum;
-	util = div64_u64(util,
-			 sched_ravg_window >> SCHED_CAPACITY_SHIFT);
+	if (!walt_disabled && sysctl_sched_use_walt_cpu_util) {
+		util = rq->prev_runnable_sum + rq->grp_time.prev_runnable_sum;
+		util = div64_u64(util,
+				 sched_ravg_window >> SCHED_CAPACITY_SHIFT);
+	}
 
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
 