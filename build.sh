1d8433eea94d9c7c4402d849a8465ca0347b0f0e
Richard Raya <rdxzv.dev@gmail.com>
2026-08-03 00:02:32 -0300

    Revert "build.sh: Always regenerate stock defconfig"
    This reverts commit 6b06ddb494bc28772908268e1e5fa0d621a49619.
    
    Change-Id: I51be363d7adee83bc390a3221fb74668eed24ec3
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- build.sh
+++ build.sh
@@ -90,8 +90,6 @@ MAKE=(
 )
 
 if [[ ${1:-} == -r || ${1:-} == --regen ]]; then
-	"${MAKE[@]}" vendor/surya-stock_defconfig savedefconfig
-	cp out/defconfig arch/arm64/configs/vendor/surya-stock_defconfig
 	"${MAKE[@]}" $DEFCONFIG savedefconfig
 	cp out/defconfig arch/arm64/configs/$DEFCONFIG
 	printf "\nSuccessfully regenerated defconfig at %s\n" $DEFCONFIG
@@ -99,8 +97,6 @@ if [[ ${1:-} == -r || ${1:-} == --regen ]]; then
 fi
 
 if [[ ${1:-} == -rf || ${1:-} == --regen-full ]]; then
-	"${MAKE[@]}" vendor/surya-stock_defconfig
-	cp out/.config arch/arm64/configs/vendor/surya-stock_defconfig
 	"${MAKE[@]}" $DEFCONFIG
 	cp out/.config arch/arm64/configs/$DEFCONFIG
 	printf "\nSuccessfully regenerated full defconfig at %s\n" $DEFCONFIG
