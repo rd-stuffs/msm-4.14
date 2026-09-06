27ac37be56cea53ca5a81ae111d5b580885668c9
Richard Raya <rdxzv.dev@gmail.com>
2026-09-05 23:30:23 -0300

    fs: Track all powerhal services concurrently
    The powerhal task pointer holds only one task_struct, so each exec
    overwrites it and only the most recently started service is
    recognized. A concurrently running service loses its logd block and
    missing-knob redirect.
    
    Fix it by storing one pointer per known binary in a fixed array and
    checking each entry in task_is_powerhal().
    
    Change-Id: I94ec697f2956544847b5b1c163be0f5680a9b8f1
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -85,18 +85,40 @@ int suid_dumpable = 0;
 #define LIBPERFMGR_LINEAGE_BIN "/vendor/bin/hw/android.hardware.power-service.lineage-libperfmgr"
 #define LIBPERFMGR_XIAOMI_BIN "/vendor/bin/hw/android.hardware.power-service.xiaomi-libperfmgr"
 
-static struct task_struct *powerhal_tsk;
+#define POWERHAL_SLOTS 6
+static struct task_struct *powerhal_tsks[POWERHAL_SLOTS];
 bool task_is_powerhal(struct task_struct *p)
 {
 	struct task_struct *tsk;
-	bool ret;
+	int i;
 
 	rcu_read_lock();
-	tsk = READ_ONCE(powerhal_tsk);
-	ret = tsk && same_thread_group(p, tsk);
+	for (i = 0; i < POWERHAL_SLOTS; i++) {
+		tsk = READ_ONCE(powerhal_tsks[i]);
+		if (tsk && same_thread_group(p, tsk)) {
+			rcu_read_unlock();
+			return true;
+		}
+	}
 	rcu_read_unlock();
 
-	return ret;
+	return false;
+}
+
+/* ponytail: fixed array, overwrite slot 0 if more than POWERHAL_SLOTS live powerhals */
+static void powerhal_track(struct task_struct *p)
+{
+	int i;
+
+	for (i = 0; i < POWERHAL_SLOTS; i++)
+		if (READ_ONCE(powerhal_tsks[i]) == p)
+			return;
+	for (i = 0; i < POWERHAL_SLOTS; i++)
+		if (!READ_ONCE(powerhal_tsks[i])) {
+			WRITE_ONCE(powerhal_tsks[i], p);
+			return;
+		}
+	WRITE_ONCE(powerhal_tsks[0], p);
 }
 
 static struct task_struct *servicemanager_tsk;
@@ -107,9 +129,14 @@ bool task_is_servicemanager(struct task_struct *p)
 
 void dead_special_task(void)
 {
-	if (unlikely(current == powerhal_tsk))
-		WRITE_ONCE(powerhal_tsk, NULL);
-	else if (unlikely(current == servicemanager_tsk))
+	int i;
+
+	for (i = 0; i < POWERHAL_SLOTS; i++)
+		if (unlikely(READ_ONCE(powerhal_tsks[i]) == current)) {
+			WRITE_ONCE(powerhal_tsks[i], NULL);
+			return;
+		}
+	if (unlikely(current == servicemanager_tsk))
 		WRITE_ONCE(servicemanager_tsk, NULL);
 }
 
@@ -1928,23 +1955,13 @@ static int __do_execve_file(int fd, struct filename *filename,
 		if (unlikely(!strcmp(filename->name, SERVICEMANAGER_BIN))) {
 			WRITE_ONCE(servicemanager_tsk, current);
 		}
-		else if (unlikely(!strcmp(filename->name, PERF_BIN))) {
-			WRITE_ONCE(powerhal_tsk, current);
-		}
-		else if (unlikely(!strcmp(filename->name, PERFD_BIN))) {
-			WRITE_ONCE(powerhal_tsk, current);
-		}
-		else if (unlikely(!strcmp(filename->name, PERFD2_BIN))) {
-			WRITE_ONCE(powerhal_tsk, current);
-		}
-		else if (unlikely(!strcmp(filename->name, IOP_BIN))) {
-			WRITE_ONCE(powerhal_tsk, current);
-		}
-		else if (unlikely(!strcmp(filename->name, LIBPERFMGR_LINEAGE_BIN))) {
-			WRITE_ONCE(powerhal_tsk, current);
-		}
-		else if (unlikely(!strcmp(filename->name, LIBPERFMGR_XIAOMI_BIN))) {
-			WRITE_ONCE(powerhal_tsk, current);
+		else if (unlikely(!strcmp(filename->name, PERF_BIN) ||
+				 !strcmp(filename->name, PERFD_BIN) ||
+				 !strcmp(filename->name, PERFD2_BIN) ||
+				 !strcmp(filename->name, IOP_BIN) ||
+				 !strcmp(filename->name, LIBPERFMGR_LINEAGE_BIN) ||
+				 !strcmp(filename->name, LIBPERFMGR_XIAOMI_BIN))) {
+			powerhal_track(current);
 		}
 		else if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN))) {
 			zygote32_sig = current->signal;
