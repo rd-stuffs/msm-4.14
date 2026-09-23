8280379d64e0ed8822ba0bebf783bf7f8c61ad67
Richard Raya <rdxzv.dev@gmail.com>
2026-09-23 08:35:00 -0300

    fingerprint: gf3208: Enable interrupt wake upon successful IRQ request
    In gf_setup(), the return value of request_threaded_irq() is checked
    without negation. Because request_threaded_irq() returns 0 on success,
    irq_switch() is only invoked when IRQ registration fails. On successful
    probe, enable_irq_wake() is never called, leaving the fingerprint sensor
    unable to wake the system from sleep.
    
    Fix it by inverting the condition so irq_switch() is called when
    request_threaded_irq() succeeds.
    
    Change-Id: I436c160cc46339e7add667ea388c1e665e400694
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/input/fingerprint/gf3208/gf_spi.c
+++ drivers/input/fingerprint/gf3208/gf_spi.c
@@ -104,7 +104,7 @@ static inline void gf_setup(struct gf_dev *gf_dev) {
 	gpio_request(gf_dev->irq_gpio, "goodix_irq");
 	gpio_direction_input(gf_dev->irq_gpio);
 	gf_dev->irq = gpio_to_irq(gf_dev->irq_gpio);
-	if (request_threaded_irq(gf_dev->irq, NULL, gf_irq,
+	if (!request_threaded_irq(gf_dev->irq, NULL, gf_irq,
 			IRQF_TRIGGER_RISING | IRQF_ONESHOT, "gf", gf_dev))
 		irq_switch(gf_dev, 1);
 	return;
